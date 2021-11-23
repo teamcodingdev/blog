324ea83afa37a6079873f18304eae05ef0c28b31
cursecan <anderi.setiawan@gmail.com>
2021-11-23 10:58:52 +0700

    Allol all origin

--- ateam_backend/settings/base.py
+++ ateam_backend/settings/base.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
+from os import truncate
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -121,6 +122,7 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = [
     "http://127.0.0.1:3000"
 ]
+CORS_ALLOW_ALL_ORIGINS = True
 
 TINYMCE_DEFAULT_CONFIG = {
     "theme": "silver",
